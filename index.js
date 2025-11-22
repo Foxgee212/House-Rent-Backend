f12de8eea09f41a6cff7447eee39f58f199b9513
Mr Shrek <ibrahymabdullahi2018@gmail.com>
2025-11-22 07:00:03 +0100

    fixing Cors issues

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const allowedOrigins = [
   "https://naijahome.ng",
   "https://www.naijahome.ng",
   "http://localhost:3000",
-  "https://naijahome-next-r8pqvvmng-foxgee212s-projects.vercel.app/",
+  "https://naijahome-next-r8pqvvmng-foxgee212s-projects.vercel.app",
 ];
 
 const corsOptions = {
